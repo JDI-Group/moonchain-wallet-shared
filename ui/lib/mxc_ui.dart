d493ddc392c07b369249862d670f44f2d16e9195
Adam Zh <zh.bh@qq.com>
2023-05-19 00:34:59 +0800

    Update mxc_ui.dart

--- ui/lib/mxc_ui.dart
+++ ui/lib/mxc_ui.dart
@@ -1,8 +1,9 @@
 library mxc_ui;
 
 export 'src/theme/theme.dart';
-export 'src/shadow.dart';
 export 'src/dialogs/dialogs.dart';
 export 'src/buttons/buttons.dart';
+export 'src/loading_barrier.dart';
+export 'src/shadow.dart';
 
 const mxcUiPackageName = 'mxc_ui';
