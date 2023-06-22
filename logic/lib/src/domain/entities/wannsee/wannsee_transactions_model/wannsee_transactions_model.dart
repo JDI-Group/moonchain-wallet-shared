316237136cd64109acbaf94129db200347d05a06
reasje <rezaaslejeddian@gmail.com>
2023-06-22 23:35:07 +0330

    feat: Added wannsee chain models

---
+++ logic/lib/src/domain/entities/wannsee/wannsee_transactions_model/wannsee_transactions_model.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:equatable/equatable.dart';
+
+import 'wannsee_transaction.dart';
+
+export 'wannsee_transaction.dart';
+
+class WannseeTransactionsModel extends Equatable {
+  final List<WannseeTransactionModel>? items;
+  final dynamic nextPageParams;
+
+  const WannseeTransactionsModel({this.items, this.nextPageParams});
+
+  factory WannseeTransactionsModel.fromMap(Map<String, dynamic> data) {
+    return WannseeTransactionsModel(
+      items: (data['items'] as List<dynamic>?)
+          ?.map((dynamic e) => WannseeTransactionModel.fromMap(e as Map<String, dynamic>))
+          .toList(),
+      nextPageParams: data['next_page_params'] as dynamic,
+    );
+  }
+
+  Map<String, dynamic> toMap() => <String, dynamic>{
+        'items': items?.map((e) => e.toMap()).toList(),
+        'next_page_params': nextPageParams,
+      };
+
+  /// `dart:convert`
+  ///
+  /// Parses the string and returns the resulting Json object as [WannseeTransactionsModel].
+  factory WannseeTransactionsModel.fromJson(String data) {
+    return WannseeTransactionsModel.fromMap(
+        json.decode(data) as Map<String, dynamic>);
+  }
+
+  /// `dart:convert`
+  ///
+  /// Converts [WannseeTransactionsModel] to a JSON string.
+  String toJson() => json.encode(toMap());
+
+  WannseeTransactionsModel copyWith({
+    List<WannseeTransactionModel>? items,
+    dynamic nextPageParams,
+  }) {
+    return WannseeTransactionsModel(
+      items: items ?? this.items,
+      nextPageParams: nextPageParams ?? this.nextPageParams,
+    );
+  }
+
+  @override
+  List<Object?> get props => [items, nextPageParams];
+}
