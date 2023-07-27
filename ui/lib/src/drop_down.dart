32e375d80eab97e870ecb921146436259a19d2b5
reasje <rezaaslejeddian@gmail.com>
2023-07-27 09:59:36 +0330

    feat: Updated mxc drop down

--- ui/lib/src/drop_down.dart
+++ ui/lib/src/drop_down.dart
@@ -1,55 +1,56 @@
 import 'package:flutter/material.dart';
 import 'package:mxc_ui/mxc_ui.dart';
 
-class MXCDropDown<T> extends StatefulWidget {
-  final List<T> itemList;
-  final void Function(T?)? onChanged;
-  final T selectedItem;
-  final Widget? icon;
-  final BorderRadius? borderRadius;
-  final Widget? leadingIcon;
-  final TextStyle? textStyle;
+class MXCDropDown extends StatelessWidget {
   const MXCDropDown(
       {super.key,
-      required this.itemList,
-      required this.onChanged,
+      required this.onTap,
       required this.selectedItem,
-      this.icon,
-      this.borderRadius,
-      this.leadingIcon,
-      this.textStyle});
+      this.selectedItemHint});
+  final Function onTap;
+  final String selectedItem;
+  final String? selectedItemHint;
 
-  @override
-  State<MXCDropDown<T>> createState() => _MXCDropDownState<T>();
-}
-
-class _MXCDropDownState<T> extends State<MXCDropDown<T>> {
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<T>(
-      value: widget.selectedItem,
-      icon: widget.icon ?? Container(),
-      underline: Container(),
-      focusColor: ColorsTheme.of(context).screenBackground,
-      dropdownColor: ColorsTheme.of(context).box,
-      elevation: 16,
-      isDense: true,
-      borderRadius: widget.borderRadius,
-      style: TextStyle(color: ColorsTheme.of(context).textPrimary),
-      onChanged: widget.onChanged,
-      items: widget.itemList
-          .map((item) => DropdownMenuItem<T>(
-                value: item,
-                child: Row(
-                  children: [
-                    widget.leadingIcon ?? Container(),
-                    Text(item.toString(),
-                        style: widget.textStyle ??
-                            FontTheme.of(context).subtitle2.primary()),
-                  ],
-                ),
-              ))
-          .toList(),
+    return Container(
+      padding: const EdgeInsetsDirectional.only(
+          top: Sizes.spaceSmall,
+          bottom: Sizes.spaceSmall,
+          end: Sizes.spaceSmall,
+          start: Sizes.spaceNormal),
+      decoration: BoxDecoration(
+          color: ColorsTheme.of(context).screenBackground,
+          border: Border.all(color: ColorsTheme.of(context).borderGrey3),
+          borderRadius: const BorderRadius.all(Radius.circular(10))),
+      child: Row(
+        children: [
+          Text(
+            selectedItem,
+            style: FontTheme.of(context).body1.primary(),
+          ),
+          const SizedBox(
+            width: Sizes.spaceXSmall,
+          ),
+          selectedItemHint != null
+              ? Text(
+                  selectedItemHint!,
+                  style: FontTheme.of(context)
+                      .body1()
+                      .copyWith(color: ColorsTheme.of(context).textWhite100),
+                )
+              : Container(),
+          const Spacer(),
+          const SizedBox(
+            width: Sizes.spaceNormal,
+          ),
+          Icon(
+            Icons.arrow_drop_down_rounded,
+            size: 24,
+            color: ColorsTheme.of(context).iconPrimary,
+          )
+        ],
+      ),
     );
   }
 }
