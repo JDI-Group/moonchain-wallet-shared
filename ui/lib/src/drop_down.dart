57d683cffb310712a8226dfa766c7ee3680e89eb
reasje <rezaaslejeddian@gmail.com>
2023-07-28 20:46:40 +0330

    feat: Updated dropdown for tapping

--- ui/lib/src/drop_down.dart
+++ ui/lib/src/drop_down.dart
@@ -13,43 +13,48 @@ class MXCDropDown extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsetsDirectional.only(
-          top: Sizes.spaceSmall,
-          bottom: Sizes.spaceSmall,
-          end: Sizes.spaceSmall,
-          start: Sizes.spaceNormal),
-      decoration: BoxDecoration(
-          color: ColorsTheme.of(context).screenBackground,
-          border: Border.all(color: ColorsTheme.of(context).borderGrey3),
-          borderRadius: const BorderRadius.all(Radius.circular(10))),
-      child: Row(
-        children: [
-          Text(
-            selectedItem,
-            style: FontTheme.of(context).body1.primary(),
-          ),
-          const SizedBox(
-            width: Sizes.spaceXSmall,
-          ),
-          selectedItemHint != null
-              ? Text(
-                  selectedItemHint!,
-                  style: FontTheme.of(context)
-                      .body1()
-                      .copyWith(color: ColorsTheme.of(context).textWhite100),
-                )
-              : Container(),
-          const Spacer(),
-          const SizedBox(
-            width: Sizes.spaceNormal,
-          ),
-          Icon(
-            Icons.arrow_drop_down_rounded,
-            size: 24,
-            color: ColorsTheme.of(context).iconPrimary,
-          )
-        ],
+    return InkWell(
+      onTap: () {
+        onTap();
+      },
+      child: Container(
+        padding: const EdgeInsetsDirectional.only(
+            top: Sizes.spaceSmall,
+            bottom: Sizes.spaceSmall,
+            end: Sizes.spaceSmall,
+            start: Sizes.spaceNormal),
+        decoration: BoxDecoration(
+            color: ColorsTheme.of(context).screenBackground,
+            border: Border.all(color: ColorsTheme.of(context).borderGrey3),
+            borderRadius: const BorderRadius.all(Radius.circular(10))),
+        child: Row(
+          children: [
+            Text(
+              selectedItem,
+              style: FontTheme.of(context).body1.primary(),
+            ),
+            const SizedBox(
+              width: Sizes.spaceXSmall,
+            ),
+            selectedItemHint != null
+                ? Text(
+                    selectedItemHint!,
+                    style: FontTheme.of(context)
+                        .body1()
+                        .copyWith(color: ColorsTheme.of(context).textWhite100),
+                  )
+                : Container(),
+            const Spacer(),
+            const SizedBox(
+              width: Sizes.spaceNormal,
+            ),
+            Icon(
+              Icons.arrow_drop_down_rounded,
+              size: 24,
+              color: ColorsTheme.of(context).iconPrimary,
+            )
+          ],
+        ),
       ),
     );
   }
