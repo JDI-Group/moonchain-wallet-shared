29323a29082ba006655ea3901a6a7f38254ecd38
Adam Zh <zh.bh@qq.com>
2023-08-04 02:23:59 +0800

    update: switch network

--- logic/lib/src/domain/repositories/web3_repository.dart
+++ logic/lib/src/domain/repositories/web3_repository.dart
@@ -1,7 +1,6 @@
+import 'package:mxc_logic/mxc_logic.dart';
 import 'package:mxc_logic/src/data/api/client/web3_client.dart';
 import 'package:mxc_logic/src/data/data.dart';
-import 'package:mxc_logic/src/data/socket/mxc_socket_client.dart';
-import 'package:mxc_logic/src/data/socket/phoenix/phoenix_client.dart';
 import 'package:mxc_logic/src/domain/repositories/wallet/nft_contract.dart';
 import 'package:mxc_logic/src/domain/repositories/wallet/token_contract.dart';
 
@@ -13,9 +12,17 @@ class Web3Repository {
   })  : _setupStore = setupStore,
         _web3client = DatadashClient(
           getRpcHttpUrl: () =>
-              setupStore.web3RpcHttpUrl ?? 'https://wannsee-rpc.mxc.com',
+              setupStore.web3RpcHttpUrl ??
+              Network.fixedNetworks()
+                  .where((item) => item.enabled)
+                  .first
+                  .web3RpcHttpUrl,
           getRpcWebsocketUrl: () =>
-              setupStore.web3RpcWebsocketUrl ?? 'https://wannsee-rpc.mxc.com',
+              setupStore.web3RpcWebsocketUrl ??
+              Network.fixedNetworks()
+                  .where((item) => item.enabled)
+                  .first
+                  .web3RpcWebsocketUrl,
         );
 
   final DatadashSetupStore _setupStore;
@@ -23,7 +30,8 @@ class Web3Repository {
 
   WalletAddressRepoistory get walletAddress => const WalletAddressRepoistory();
 
-  TokenContractRepository get tokenContract => TokenContractRepository(_web3client);
+  TokenContractRepository get tokenContract =>
+      TokenContractRepository(_web3client);
 
   NftContractRepository get nftContract => NftContractRepository(_web3client);
 }
