077cacb3c876132371289fe6a3576e5f178e487d
Adam Zh <zh.bh@qq.com>
2023-08-16 22:17:26 +0800

    fix: Cache Issue in application

--- logic/lib/src/domain/entities/account.dart
+++ logic/lib/src/domain/entities/account.dart
@@ -1,11 +1,13 @@
 class Account {
   Account({
     required this.name,
+    required this.privateKey,
     required this.address,
     this.mns,
   });
 
   String name;
+  String privateKey;
   String address;
   String? mns;
 }
--- logic/lib/src/domain/repositories/auth/auth_cache_repository.dart
+++ logic/lib/src/domain/repositories/auth/auth_cache_repository.dart
@@ -1,11 +1,21 @@
 import 'package:mxc_logic/internal.dart';
+import 'package:mxc_logic/mxc_logic.dart';
 
 class AuthenticationCacheRepository {
-  AuthenticationCacheRepository(this._controller);
+  AuthenticationCacheRepository(
+    this._controller,
+    this._authStorageRepository,
+  );
 
   final CacheController _controller;
+  final AuthenticationStorageRepository _authStorageRepository;
 
-  Future<void> loadCache(String username) async {
+  Future<void> loadCache() async {
+    final currentNetwork = _authStorageRepository.network ??
+        Network.fixedNetworks().where((item) => item.enabled).first;
+
+    final username =
+        '${currentNetwork.label?.split(' ').join()}_${_authStorageRepository.publicAddress}';
     await _controller.load(username);
   }
 }
