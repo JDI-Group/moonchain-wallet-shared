921401b5deed503e329b6543ee508483af98274d
reasje <rezaaslejeddian@gmail.com>
2023-10-13 14:11:30 +0330

    feaT: Added isCustom to Account for better account detection

--- logic/lib/src/domain/entities/account.dart
+++ logic/lib/src/domain/entities/account.dart
@@ -4,10 +4,15 @@ class Account {
     required this.privateKey,
     required this.address,
     this.mns,
+    required this.isCustom,
+    // this.alias
   });
 
   String name;
   String privateKey;
   String address;
   String? mns;
+  /// If -1 then It is the import account
+  bool isCustom;
+  // String? alias;
 }
