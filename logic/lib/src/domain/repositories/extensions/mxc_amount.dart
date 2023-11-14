55a0977744b1895ba152f932a22546545e176c2d
reasje <rezaaslejeddian@gmail.com>
2023-11-14 17:42:59 +0330

    feat: Added fromDoubleByGWei

--- logic/lib/src/domain/repositories/extensions/mxc_amount.dart
+++ logic/lib/src/domain/repositories/extensions/mxc_amount.dart
@@ -5,13 +5,13 @@ import 'package:web3dart/web3dart.dart';
 class MxcAmount extends EtherAmount {
   MxcAmount.inWei(super.value) : super.inWei();
 
-  static EtherAmount fromDoubleByEther(double value) {
+  static EtherAmount fromDoubleByGWei(double value) {
     final wei = BigInt.from(10).pow(9).toDouble() * value;
 
     return EtherAmount.inWei(BigInt.from(wei));
   }
 
-  static EtherAmount fromDoubleByGWei(double value) {
+  static EtherAmount fromDoubleByEther(double value) {
     final wei = BigInt.from(10).pow(18).toDouble() * value;
 
     return EtherAmount.inWei(BigInt.from(wei));
