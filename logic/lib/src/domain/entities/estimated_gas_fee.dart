64b397c36d6aeb01b25311c2a9dbda58854530b5
reasje <rezaaslejeddian@gmail.com>
2023-12-08 15:17:23 +0330

    feat: new priority fees model

--- logic/lib/src/domain/entities/estimated_gas_fee.dart
+++ logic/lib/src/domain/entities/estimated_gas_fee.dart
@@ -7,7 +7,7 @@ class TransactionGasEstimation {
     required this.gasFee,
   });
 
-  /// gas price for each unit of gas 
+  /// gas price for each unit of gas
   /// We get from dApp or we get we get from web3dart getGasPrice
   final EtherAmount gasPrice;
 
@@ -15,6 +15,17 @@ class TransactionGasEstimation {
   final BigInt gas;
 
   /// Total gas fee required for tx.
-  /// calculation : gasPrice * gas 
+  /// calculation : gasPrice * gas
   final double gasFee;
+}
+
+class TransactionPriorityFeeEstimation {
+  const TransactionPriorityFeeEstimation(
+      {required this.maxFeePerGas, required this.maxPriorityFeePerGas,});
+
+  /// The maximum fee user is willing to spend extra on each unit of gas
+  final double maxPriorityFeePerGas;
+
+  /// The maximum fee user willing to spend on each unit of gas
+  final double maxFeePerGas;
 }
