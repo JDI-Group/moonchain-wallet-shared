2f270179f382a6a250bd22bd1335d36ce8987ee5
reasje <rezaaslejeddian@gmail.com>
2023-12-12 14:24:45 +0330

    imprv: copy with function for gas estimation models

--- logic/lib/src/domain/entities/estimated_gas_fee.dart
+++ logic/lib/src/domain/entities/estimated_gas_fee.dart
@@ -17,15 +17,41 @@ class TransactionGasEstimation {
   /// Total gas fee required for tx.
   /// calculation : gasPrice * gas
   final double gasFee;
+
+  /// Returns a copy of this object with the specified fields updated.
+  TransactionGasEstimation copyWith({
+    EtherAmount? gasPrice,
+    BigInt? gas,
+    double? gasFee,
+  }) {
+    return TransactionGasEstimation(
+      gasPrice: gasPrice ?? this.gasPrice,
+      gas: gas ?? this.gas,
+      gasFee: gasFee ?? this.gasFee,
+    );
+  }
 }
 
 class TransactionPriorityFeeEstimation {
-  const TransactionPriorityFeeEstimation(
-      {required this.maxFeePerGas, required this.maxPriorityFeePerGas,});
+  const TransactionPriorityFeeEstimation({
+    required this.maxFeePerGas,
+    required this.maxPriorityFeePerGas,
+  });
 
   /// The maximum fee user is willing to spend extra on each unit of gas
   final double maxPriorityFeePerGas;
 
   /// The maximum fee user willing to spend on each unit of gas
   final double maxFeePerGas;
+
+  /// Returns a copy of this object with the specified fields updated.
+  TransactionPriorityFeeEstimation copyWith({
+    double? maxFeePerGas,
+    double? maxPriorityFeePerGas,
+  }) {
+    return TransactionPriorityFeeEstimation(
+      maxFeePerGas: maxFeePerGas ?? this.maxFeePerGas,
+      maxPriorityFeePerGas: maxPriorityFeePerGas ?? this.maxPriorityFeePerGas,
+    );
+  }
 }
