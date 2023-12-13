beefb769769d29720c4af88a9646ec52c09139b5
reasje <rezaaslejeddian@gmail.com>
2023-12-13 17:15:44 +0330

    imrpv: Moved formatter to shared

--- logic/lib/src/domain/utils/mxc_validation.dart
+++ logic/lib/src/domain/utils/mxc_validation.dart
@@ -1,6 +1,19 @@
-class MXCValidation{ 
-    static bool isExpoNumber(String input) {
+import 'package:mxc_logic/mxc_logic.dart';
+import 'package:flutter/material.dart';
+import 'package:web3dart/web3dart.dart';
+
+class MXCValidation {
+  static bool isExpoNumber(String input) {
     RegExp regex = RegExp(r'^(\d+\.\d+e[-+]\d+)$');
     return regex.hasMatch(input);
+  }
+
+  static bool isDouble(String value) {
+    final doubleValue = double.tryParse(value);
+
+    if (doubleValue == null) {
+      return false;
+    }
+    return true;
   }
 }
