fcaaaa4d3d6a3f562a2fa461752a9daa4d8a3157
reasje <rezaaslejeddian@gmail.com>
2024-02-07 10:19:15 +0330

    feat: time to minutes util

--- logic/lib/src/domain/utils/mxc_type.dart
+++ logic/lib/src/domain/utils/mxc_type.dart
@@ -1,5 +1,6 @@
 import 'dart:typed_data';
 
+import 'package:flutter/material.dart';
 import 'package:web3dart/crypto.dart';
 
 class MXCType {
@@ -22,5 +23,9 @@ class MXCType {
   static Uint8List stringToUint8List(String value) {
     List<int> utf8Bytes = value.codeUnits;
     return Uint8List.fromList(utf8Bytes);
+  }
+
+  static int timeOfDayInMinutes(TimeOfDay time) {
+    return time.hour * 60 + time.minute;
   }
 }
