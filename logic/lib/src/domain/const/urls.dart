69b0ea214420b80f4bf818eb3d6f79d459b511f7
reasje <rezaaslejeddian@gmail.com>
2024-02-13 22:52:02 +0330

    refactor: remove GPT bot url

--- logic/lib/src/domain/const/urls.dart
+++ logic/lib/src/domain/const/urls.dart
@@ -55,7 +55,6 @@ class Urls {
   static const String emailApp = 'mailto:';
 
   // MXC support
-  static const String mxcChatGPT = 'http://t.me/mxcchatgpt_bot';
   static const String mxcZendesk = 'https://mxcfoundation.zendesk.com/hc/en-gb';
   static const String mxcKnowledgeHub = 'https://www.mxc.org/blog#technology';
   static const String mxcDesignDocs = 'https://doc.mxc.com/docs/intro';
