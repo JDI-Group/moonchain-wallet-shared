70530515613675370c967937d4d289e49cd425d1
reasje <rezaaslejeddian@gmail.com>
2024-03-12 06:55:00 +0330

    fix: remove events in ending

--- logic/lib/src/domain/const/js_channel/js_channel_events.dart
+++ logic/lib/src/domain/const/js_channel/js_channel_events.dart
@@ -1,11 +1,11 @@
 class JSChannelEvents {
   static const String axsReadyEvent = 'axsReady';
 
-  static const String changeCronTransitionEvent = 'changeCronTransitionEvent';
+  static const String changeCronTransitionEvent = 'changeCronTransition';
   static const String changeCronTransitionStatusEvent =
-      'changeCronTransitionStatusEvent';
-  static const String getSystemInfoEvent = 'getSystemInfoEvent';
-  static const String goToAdvancedSettingsEvent = 'goToAdvancedSettingsEvent';
+      'changeCronTransitionStatus';
+  static const String getSystemInfoEvent = 'getSystemInfo';
+  static const String goToAdvancedSettingsEvent = 'goToAdvancedSettings';
 
   static const String axsWalletCopyClipboard = 'axs-wallet-copy-clipboard';
   static const String axsWalletScrollDetector = 'axs-wallet-scroll-detector';
