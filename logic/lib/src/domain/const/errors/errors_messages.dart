aee41c066989e0e6d33a19d75b8eba6bb76bc91b
reasje <rezaaslejeddian@gmail.com>
2024-04-02 11:04:41 +0330

    refactor: Error messages const

---
+++ logic/lib/src/domain/const/errors/errors_messages.dart
@@ -0,0 +1,23 @@
+class ErrorMessages {
+  /// If error happens with these messages then we will need to show receive bottom sheet
+  static List<String> fundErrors = [
+    // User doesn't have enough to pay for native token transfer
+    // Zero native token balance or not enough for fee
+    'gas required exceeds allowance',
+    // Sending more than tokens balance
+    'execution reverted: ERC20: transfer amount exceeds balance',
+    // Sending more than native token balance
+    'insufficient funds for gas * price + value',
+    'insufficient funds for transfer'
+  ];
+
+  static List<String> errorList = [nonceTooLowError];
+
+  static String nonceTooLowError = 'nonce too low';
+
+  static Map<String, String> errorMessageMapper = {
+    nonceTooLowError: 'transaction_finalized'
+  };
+
+  static List<String> ignoredErrors = ['already known'];
+}
