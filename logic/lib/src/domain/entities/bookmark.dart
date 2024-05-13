ffa2eb84f03f41446c1a3196c26e50ffbccc4f92
reasje <rezaaslejeddian@gmail.com>
2024-05-13 08:25:32 +0330

    feat: Add copy with to bookmark

--- logic/lib/src/domain/entities/bookmark.dart
+++ logic/lib/src/domain/entities/bookmark.dart
@@ -11,6 +11,21 @@ class Bookmark extends Dapp {
     this.image,
   });
 
+  Bookmark copyWithBookmark({
+    int? id,
+    String? title,
+    String? url,
+    String? description,
+    String? image,
+  }) =>
+      Bookmark(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        url: url ?? this.url,
+        description: description ?? this.description,
+        image: image ?? this.image,
+      );
+
   final int id;
   final String title;
   final String url;
