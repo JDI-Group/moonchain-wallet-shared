5482ba4075801c6d951b7cd73be66f7bb22a2be8
reasje <rezaaslejeddian@gmail.com>
2024-06-26 08:27:08 +0330

    feat: Complete all bluetooth events

--- logic/lib/src/domain/const/js_channel/js_channel_events.dart
+++ logic/lib/src/domain/const/js_channel/js_channel_events.dart
@@ -13,21 +13,32 @@ class JSChannelEvents {
   // bluetooth events 
   static const String requestDevice = 'requestDevice';
   
-  // bluetooth objects events
+  // Bluetooth objects events
+
   // BluetoothDevice
   static const String bluetoothDeviceWatchAdvertisements = 'BluetoothDevice.watchAdvertisements';
   static const String bluetoothDeviceForget = 'BluetoothDevice.forget';
 
   // BluetoothRemoteGATTService
   static const String bluetoothRemoteGATTServiceGetCharacteristic = 'BluetoothRemoteGATTService.getCharacteristic';
+  static const String bluetoothRemoteGATTServiceGetCharacteristics = 'BluetoothRemoteGATTService.getCharacteristics';
+  static const String bluetoothRemoteGATTServiceGetIncludedService = 'BluetoothRemoteGATTService.getIncludedService';
+  static const String bluetoothRemoteGATTServiceGetIncludedServices = 'BluetoothRemoteGATTService.getIncludedServices';
 
   // BluetoothRemoteGATTServer
   static const String bluetoothRemoteGATTServerConnect ='BluetoothRemoteGATTServer.connect';
+  static const String bluetoothRemoteGATTServerDisconnect ='BluetoothRemoteGATTServer.disconnect';
   static const String bluetoothRemoteGATTServerGetPrimaryService = 'BluetoothRemoteGATTServer.getPrimaryService';
-  // .disconnect
-  // .getPrimaryServices
+  static const String bluetoothRemoteGATTServerGetPrimaryServices = 'BluetoothRemoteGATTServer.getPrimaryServices';
 
   // BluetoothRemoteGATTCharacteristic
   static const String bluetoothRemoteGATTCharacteristicStartNotifications = 'BluetoothRemoteGATTCharacteristic.startNotifications';
   static const String bluetoothRemoteGATTCharacteristicStopNotifications = 'BluetoothRemoteGATTCharacteristic.stopNotifications';
+  static const String bluetoothRemoteGATTCharacteristicGetDescriptor = 'BluetoothRemoteGATTCharacteristic.getDescriptor';
+  static const String bluetoothRemoteGATTCharacteristicGetDescriptors = 'BluetoothRemoteGATTCharacteristic.getDescriptors';
+  static const String bluetoothRemoteGATTCharacteristicReadValue = 'BluetoothRemoteGATTCharacteristic.readValue';
+  static const String bluetoothRemoteGATTCharacteristicWriteValue = 'BluetoothRemoteGATTCharacteristic.writeValue';
+  static const String bluetoothRemoteGATTCharacteristicWriteValueWithResponse = 'BluetoothRemoteGATTCharacteristic.writeValueWithResponse';
+  static const String bluetoothRemoteGATTCharacteristicWriteValueWithoutResponse = 'BluetoothRemoteGATTCharacteristic.writeValueWithoutResponse';
+  
 }
