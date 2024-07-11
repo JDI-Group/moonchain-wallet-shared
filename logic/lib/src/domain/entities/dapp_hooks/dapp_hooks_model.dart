094692644e4b254006b7757eece9253739e6cc18
reasje <rezaaslejeddian@gmail.com>
2024-07-11 18:58:59 +0330

    refactor: Update models entities

--- logic/lib/src/domain/entities/dapp_hooks/dapp_hooks_model.dart
+++ logic/lib/src/domain/entities/dapp_hooks/dapp_hooks_model.dart
@@ -10,24 +10,18 @@ class DAppHooksModel {
 
   factory DAppHooksModel.fromMap(Map<String, dynamic> map) {
     return DAppHooksModel(
-      enabled: map['enabled'] ?? false,
-      duration: map['duration'] as int? ?? 0,
       wifiHooks: WifiHooksModel.fromMap(map['wifiHooks']),
       minerHooks: MinerHooksModel.fromMap(map['minerHooks']),
     );
   }
 
   DAppHooksModel({
-    required this.enabled,
-    required this.duration,
     required this.wifiHooks,
     required this.minerHooks,
   });
 
   factory DAppHooksModel.getDefault() => DAppHooksModel(
-        enabled: false,
-        duration: 15,
-        wifiHooks: WifiHooksModel(enabled: false),
+        wifiHooks: WifiHooksModel(enabled: false, duration: 15),
         minerHooks: MinerHooksModel(
           enabled: false,
           time: BackgroundExecutionConfig.defaultTimeForMinerDapp,
@@ -35,21 +29,14 @@ class DAppHooksModel {
         ),
       );
 
-  bool enabled;
-  // Periodical delay duration in minutes
-  int duration;
   WifiHooksModel wifiHooks;
   MinerHooksModel minerHooks;
 
   DAppHooksModel copyWith({
-    bool? enabled,
-    int? duration,
     WifiHooksModel? wifiHooks,
     MinerHooksModel? minerHooks,
   }) {
     return DAppHooksModel(
-      enabled: enabled ?? this.enabled,
-      duration: duration ?? this.duration,
       wifiHooks: wifiHooks ?? this.wifiHooks,
       minerHooks: minerHooks ?? this.minerHooks,
     );
@@ -57,8 +44,6 @@ class DAppHooksModel {
 
   Map<String, dynamic> toMap() {
     return {
-      'enabled': enabled,
-      'duration': duration,
       'wifiHooks': wifiHooks.toMap(),
       'minerHooks': minerHooks.toMap(),
     };
@@ -68,7 +53,7 @@ class DAppHooksModel {
 
   @override
   String toString() {
-    return 'DAppHooksModel(enabled: $enabled, duration: $duration, wifiHooks: $wifiHooks, minerHooks: $minerHooks)';
+    return 'DAppHooksModel(wifiHooks: $wifiHooks, minerHooks: $minerHooks)';
   }
 
   @override
@@ -76,17 +61,12 @@ class DAppHooksModel {
     if (identical(this, other)) return true;
 
     return other is DAppHooksModel &&
-        other.enabled == enabled &&
-        other.duration == duration &&
         other.wifiHooks == wifiHooks &&
         other.minerHooks == minerHooks;
   }
 
   @override
   int get hashCode {
-    return enabled.hashCode ^
-        duration.hashCode ^
-        wifiHooks.hashCode ^
-        minerHooks.hashCode;
+    return wifiHooks.hashCode ^ minerHooks.hashCode;
   }
 }
--- logic/lib/src/domain/entities/dapp_hooks/wifi_hooks_model.dart
+++ logic/lib/src/domain/entities/dapp_hooks/wifi_hooks_model.dart
@@ -6,40 +6,50 @@ class WifiHooksModel {
 
   factory WifiHooksModel.fromMap(Map<String, dynamic> map) {
     return WifiHooksModel(
+      duration: map['duration'] as int? ?? 15,
       enabled: map['enabled'] ?? false,
     );
   }
   WifiHooksModel({
     required this.enabled,
+    required this.duration,
   });
+
   bool enabled;
+  // Periodical delay duration in minutes
+  int duration;
 
   WifiHooksModel copyWith({
     bool? enabled,
+    int? duration,
   }) {
     return WifiHooksModel(
       enabled: enabled ?? this.enabled,
+      duration: duration ?? this.duration,
     );
   }
 
   Map<String, dynamic> toMap() {
     return {
       'enabled': enabled,
+      'duration': duration,
     };
   }
 
   String toJson() => json.encode(toMap());
 
   @override
-  String toString() => 'WifiHooksModel(enabled: $enabled)';
+  String toString() => 'WifiHooksModel(enabled: $enabled, duration: $duration)';
 
   @override
   bool operator ==(Object other) {
     if (identical(this, other)) return true;
 
-    return other is WifiHooksModel && other.enabled == enabled;
+    return other is WifiHooksModel &&
+        other.enabled == enabled &&
+        other.duration == duration;
   }
 
   @override
-  int get hashCode => enabled.hashCode;
+  int get hashCode => enabled.hashCode ^ duration.hashCode;
 }
