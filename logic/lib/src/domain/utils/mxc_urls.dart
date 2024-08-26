12c3c8a0fc3d9e7d3c0d178384d63c6542e9d70f
reasje <rezaaslejeddian@gmail.com>
2024-08-26 11:29:57 +0330

    feat: Add MXC urls util

--- logic/lib/src/domain/utils/mxc_urls.dart
+++ logic/lib/src/domain/utils/mxc_urls.dart
@@ -1,1 +1,10 @@
-class MXCUrls {}
+class MXCUrls {
+  static String extractCIDFromUrl(String ipfsUrl) {
+    if (ipfsUrl.startsWith('ipfs://')) {
+      String cid = ipfsUrl.substring(7);
+      return cid;
+    } else {
+      throw ('Invalid IPFS URL');
+    }
+  }
+}
