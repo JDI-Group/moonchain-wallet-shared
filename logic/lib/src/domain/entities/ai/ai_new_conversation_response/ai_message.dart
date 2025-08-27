1cdb2752b8717de25388b7797ae712a4ed4751bc
reasje <rezaaslejeddian@gmail.com>
2025-08-27 08:30:57 +0330

    feat: Update ai message model with uuid + created At

--- logic/lib/src/domain/entities/ai/ai_new_conversation_response/ai_message.dart
+++ logic/lib/src/domain/entities/ai/ai_new_conversation_response/ai_message.dart
@@ -1,18 +1,35 @@
-class AIMessage {
-  AIMessage({this.content, this.role});
-  String? content;
-  String? role;
+import 'package:uuid/uuid.dart';
+import 'package:equatable/equatable.dart';
 
+class AIMessage extends Equatable {
   factory AIMessage.fromJson(Map<String, dynamic> json) => AIMessage(
-        content: json['content'] as String?,
-        role: json['role'] as String?,
+        content: (json['content'] ?? '') as String,
+        role: (json['role'] ?? '') as String,
+        createdAt: json['createdAt'] != null
+            ? DateTime.fromMillisecondsSinceEpoch(json['createdAt'])
+            : DateTime.now(),
+        uuid: json['uuid'] ?? const Uuid().v8(),
       );
+  AIMessage({
+    String? uuid,
+    required this.content,
+    required this.role,
+    DateTime? createdAt,
+  })  : uuid = uuid ?? const Uuid().v8(),
+        createdAt = createdAt ?? DateTime.now();
+
+  final String content;
+  final String role;
+  final String uuid;
+  final DateTime createdAt;
 
   Map<String, dynamic> toJson() => {
         'content': content,
         'role': role,
+        'uuid': uuid,
+        'createdAt': createdAt.millisecondsSinceEpoch,
       };
 
   @override
-  List<Object?> get props => [content, role];
+  List<Object?> get props => [uuid, content, role, createdAt];
 }
