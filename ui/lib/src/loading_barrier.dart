5e490ded00e41e6a4d8fb18af5e95abaa3aea517
reasje <rezaaslejeddian@gmail.com>
2025-10-28 16:28:02 +0330

    feat: Make overlay full screen always

--- ui/lib/src/loading_barrier.dart
+++ ui/lib/src/loading_barrier.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:mxc_ui/mxc_ui.dart';
 
-class LoadingBarrier extends StatelessWidget {
+class LoadingBarrier extends StatefulWidget {
   const LoadingBarrier({
     super.key,
     required this.isLoading,
@@ -13,15 +14,49 @@ class LoadingBarrier extends StatelessWidget {
   final Widget child;
 
   @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        IgnorePointer(
-          ignoring: isLoading,
-          child: child,
-        ),
-        if (isLoading)
-          Align(
+  State<LoadingBarrier> createState() => _LoadingBarrierState();
+}
+
+class _LoadingBarrierState extends State<LoadingBarrier> {
+  OverlayEntry? _overlayEntry;
+
+  @override
+  void initState() {
+    super.initState();
+    // Show overlay immediately if isLoading is true initially
+    if (widget.isLoading) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        _showOverlay();
+      });
+    }
+  }
+
+  @override
+  void didUpdateWidget(LoadingBarrier oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    
+    // Use post-frame callback to avoid build phase issues
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      // Show overlay when isLoading becomes true
+      if (widget.isLoading && _overlayEntry == null) {
+        _showOverlay();
+      } 
+      // Hide overlay when isLoading becomes false
+      else if (!widget.isLoading && _overlayEntry != null) {
+        _hideOverlay();
+      }
+    });
+  }
+
+  void _showOverlay() {
+    // Safety check to avoid duplicate overlays
+    if (_overlayEntry != null) return;
+    
+    _overlayEntry = OverlayEntry(
+      builder: (context) => Positioned.fill(
+        child: ColoredBox(
+          color: Colors.transparent,
+          child: Center(
             child: SizedBox(
               width: 50,
               height: 50,
@@ -31,7 +66,26 @@ class LoadingBarrier extends StatelessWidget {
               ),
             ),
           ),
-      ],
+        ),
+      ),
     );
+    
+    Overlay.of(context).insert(_overlayEntry!);
+  }
+
+  void _hideOverlay() {
+    _overlayEntry?.remove();
+    _overlayEntry = null;
+  }
+
+  @override
+  void dispose() {
+    _hideOverlay();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return widget.child;
   }
 }
